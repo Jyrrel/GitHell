d716b00efe821d854934a6f051d9611c477f9bc8
Jay Patrick Cano <0x3EF8@gmail.com>
2024-12-20 07:03:28 +0800

    Update Doom.java

--- Doom.java
+++ Doom.java
@@ -4,24 +4,22 @@ import java.util.Date;
 public class Doom {
 
     public static void main(String[] args) {
+        int numberOfCommits = 100;  
+
         try {
             File folder = new File("gen");
             if (!folder.exists()) {
                 folder.mkdir();
             }
-      
-            for (int i = 1; i <= 100; i++) {
-                File file = new File("gen/abyss" + i + ".txt");
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                    writer.write("Generated at: " + new Date().toString() + " - HAHAHAHA! Welcome to the abyss " + i);
-                    System.out.println("The abyss " + i + " has been forged in the darkest depths: " + file.getAbsolutePath());
-                }
-                logToFile("git-commit-log.txt", "Created abyss" + i + ".txt at: " + new Date().toString());
-            }
 
-            File readmeFile = new File("gen/README.yml");
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(readmeFile))) {
-                writer.write("HAHAHAHAHHA - The abyss grows larger, every minute it consumes more...");
+            for (int i = 0; i < numberOfCommits; i++) {
+                File file = new File("gen/README.yml");
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                    writer.write("Generated at: " + new Date().toString() + " - Time’s up. The abyss consumes all. Commit #" + (i+1) + "\n");
+                    System.out.println("The file has been forged in the darkest depths of GitHell: " + file.getAbsolutePath());
+                }
+
+                logToFile("git-commit-log.txt", "Commit made at: " + new Date().toString() + " - The end draws closer. Commit #" + (i+1));
             }
 
         } catch (Exception e) {
@@ -30,10 +28,12 @@ public class Doom {
     }
 
     private static void logToFile(String filename, String message) {
-        try (FileWriter logWriter = new FileWriter(filename, true);
-             BufferedWriter logBuffer = new BufferedWriter(logWriter)) {
+        try {
+            FileWriter logWriter = new FileWriter(filename, true);
+            BufferedWriter logBuffer = new BufferedWriter(logWriter);
             logBuffer.write(message);
             logBuffer.newLine();
+            logBuffer.close();
         } catch (IOException e) {
             System.err.println("Error logging commit: " + e.getMessage());
         }
